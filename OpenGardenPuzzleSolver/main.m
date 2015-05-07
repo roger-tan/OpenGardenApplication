fcc8d25f463fbd17d2c55247ea08d149d7073164
Roger TAN <rogertan@Rogers-MacBook-Pro.local>
2015-05-06 21:41:50 -0700

    Initial Commit

---
+++ OpenGardenPuzzleSolver/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OpenGardenPuzzleSolver
+//
+//  Created by Roger TAN on 06/05/15.
+//  Copyright (c) 2015 Roger TAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
